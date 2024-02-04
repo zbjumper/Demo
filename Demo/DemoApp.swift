84d3abd105130425c0bc0a2b708a9aecd22c9164
张博 <zbjumper@gmail.com>
2024-02-04 18:29:28 +0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by 张博 on 2024/2/4.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
